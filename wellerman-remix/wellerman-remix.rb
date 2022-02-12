3a366c05dd930e3c4e262bed2ebd873ad90d5eda
Ruby Adkins <radkins22@students.stab.org>
2022-02-12 01:54:01 -0500

    add chorus

--- wellerman-remix/wellerman-remix.rb
+++ wellerman-remix/wellerman-remix.rb
@@ -1,5 +1,6 @@
 use_bpm 192
 
+#verse
 8.times do
   play chord(:A, :minor)
   sleep 1
@@ -19,6 +20,36 @@ end
 
 4.times do
   play chord(:E, :major)
+  sleep 1
+  play chord(:A, :minor)
+  sleep 1
+end
+
+#chorus
+sleep 2
+play chord(:F, :major), sustain: 2
+sleep 3
+play chord(:C, :major)
+sleep 1
+play chord(:C, :major)
+sleep 1
+
+6.times do
+  play chord(:D, :minor)
+  sleep 1
+  play chord(:A, :minor)
+  sleep 1
+end
+
+play chord(:F, :major), sustain: 2
+sleep 3
+play chord(:C, :major)
+sleep 1
+play chord(:C, :major)
+sleep 1
+
+4.times do
+  play chord(:E, :minor)
   sleep 1
   play chord(:A, :minor)
   sleep 1
